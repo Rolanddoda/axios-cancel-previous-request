9ba62bf1aff1f4a3fcb0f150b3a2814fb7391c28
Roland Doda <rolanddoda2014@gmail.com>
2020-02-16 16:52:01 +0100

    Add logic

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,84 @@
 </template>
 
 <script>
+import axios from "axios";
+// Components
 import LeftSection from "./components/LeftSection";
+
+const API_URL = "https://reqres.in/api/users?delay=2";
 
 export default {
   name: "App",
 
   components: {
     LeftSection
+  },
+
+  data: () => ({
+    requests: [],
+    id: 0
+  }),
+
+  computed: {
+    latest() {
+      if (!this.requests.length) return null;
+      return this.requests[this.requests.length - 1];
+    },
+
+    done: {
+      get() {
+        if (!this.latest) return undefined;
+        return this.latest.done;
+      },
+
+      set(val) {
+        const lastIndex = this.requests.length - 1;
+        this.requests.splice(lastIndex, 1, {
+          ...this.latest,
+          done: val
+        });
+      }
+    }
+  },
+
+  methods: {
+    getNewRequest() {
+      this.id++;
+      const request = axios.CancelToken.source();
+      return { id: this.id, done: null, request };
+    },
+
+    send() {
+      this.latest && this.latest.done === null && this.cancel();
+      this.makeRequest();
+    },
+
+    makeRequest() {
+      this.requests.push(this.getNewRequest());
+      this.$refs.rightSection.scrollToBottom();
+      axios
+        .get(API_URL, { cancelToken: this.latest.request.token })
+        .then(() => (this.done = true))
+        .catch(this.logResponseErrors);
+    },
+
+    logResponseErrors(err) {
+      if (axios.isCancel(err)) {
+        console.warn("Request cancelled by the user");
+      } else {
+        console.log("Another error happened");
+      }
+    },
+
+    cancel() {
+      this.latest.request.cancel("Optional message");
+      this.done = false;
+    },
+
+    resetRequests() {
+      this.requests = [];
+      this.id = 0;
+    }
   }
 };
 </script>
