264f65ab09cf2e8d92ee5a554cc602697749052d
Roland Doda <rolanddoda2014@gmail.com>
2020-03-08 17:02:49 +0100

    Add script to deploy in github pages

---
+++ scripts/gh-pages-deploy.js
@@ -0,0 +1,30 @@
+const execa = require("execa");
+const emoji = require("node-emoji");
+const chalk = require("chalk");
+
+const firstLog = emoji.get("fast_forward") + " " + chalk.yellow("Building...");
+const secondLog = emoji.get("fast_forward") + " " + chalk.yellow("Pushing...");
+const thirdLog =
+  emoji.get("rocket") +
+  " " +
+  chalk.green("Your app successfully deployed") +
+  " " +
+  emoji.get("rocket");
+
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    console.log(firstLog);
+    await execa("npm", ["run", "build"]);
+    await execa("git", ["--work-tree", "dist", "add", "--all"]);
+    await execa("git", ["--work-tree", "dist", "commit", "-m", "gh-pages"]);
+    console.log(secondLog);
+    await execa("git", ["push", "-f", "origin", "gh-pages"]);
+    await execa("rm", ["-r", "dist"]);
+    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log(thirdLog);
+  } catch (e) {
+    console.log(e.message);
+  }
+})();
