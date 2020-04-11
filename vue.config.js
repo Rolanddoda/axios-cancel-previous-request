6c5ecf025799a9d4264703c31923d8d1863b4ec4
Roland Doda <rolanddoda2014@gmail.com>
2020-04-11 18:20:00 +0200

    Alter vue.config.js to not break app and not show loading

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,10 @@ module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
       ? "/axios-cancel-previous-request/"
-      : "/"
+      : "/",
+
+  devServer: {
+    clientLogLevel: "warn",
+    overlay: false
+  }
 };
