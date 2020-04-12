47680fe8d1fd6c458851bcd3fba48bebb55c1d48
Roland Doda <rolanddoda2014@gmail.com>
2020-04-12 12:08:53 +0200

    Simplify App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <div id="app">
-    <LeftSection
-      :done="done"
-      @send="send"
-      @cancel="cancel"
-      @reset="resetRequests"
-    />
-    <RightSection ref="rightSection" :requests="requests" />
+    <LeftSection />
+    <RightSection />
   </div>
 </template>
 
@@ -26,70 +21,9 @@ export default {
     RightSection
   },
 
-  data: () => ({
-    requests: [],
-    id: 0
-  }),
-
-  computed: {
-    latest() {
-      if (!this.requests.length) return null;
-      return this.requests[this.requests.length - 1];
-    },
-
-    done: {
-      get() {
-        if (!this.latest) return undefined;
-        return this.latest.done;
-      },
-
-      set(val) {
-        const lastIndex = this.requests.length - 1;
-        this.requests.splice(lastIndex, 1, {
-          ...this.latest,
-          done: val
-        });
-      }
-    }
-  },
-
   methods: {
-    getNewRequest() {
-      this.id++;
-      const request = axios.CancelToken.source();
-      return { id: this.id, done: null, request };
-    },
-
     send() {
-      this.latest && this.latest.done === null && this.cancel();
-      this.makeRequest();
-    },
-
-    makeRequest() {
-      this.requests.push(this.getNewRequest());
-      this.$refs.rightSection.scrollToBottom();
-      axios
-        .get(API_URL, { cancelToken: this.latest.request.token })
-        .then(() => (this.done = true))
-        .catch(this.logResponseErrors);
-    },
-
-    logResponseErrors(err) {
-      if (axios.isCancel(err)) {
-        console.warn("Request cancelled by the user");
-      } else {
-        console.log("Another error happened");
-      }
-    },
-
-    cancel() {
-      this.latest.request.cancel("Optional message");
-      this.done = false;
-    },
-
-    resetRequests() {
-      this.requests = [];
-      this.id = 0;
+      axios.get(API_URL);
     }
   }
 };
