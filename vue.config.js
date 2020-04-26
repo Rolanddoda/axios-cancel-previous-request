07302508fe646216f5d862da4b2690d8525e4981
Roland Doda <rolanddoda2014@gmail.com>
2020-04-26 19:19:15 +0200

    Remove manual deployment files

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/axios-cancel-previous-request/"
-      : "/",
-
   devServer: {
     clientLogLevel: "warn",
     overlay: false
