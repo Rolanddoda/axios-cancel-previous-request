024b42ad2ac00eec560c95761998295f4479a9dc
Roland Doda <rolanddoda2014@gmail.com>
2020-04-27 21:17:16 +0200

    Add `vue-cli-plugin-gh-pages-auto-deploy`

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/axios-cancel-previous-request/"
+      : "/",
   devServer: {
     clientLogLevel: "warn",
     overlay: false
